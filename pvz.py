b8b745179f85f3e8309c45a846c31da01549d879
Vojta654 <vojta.kristek05@gmail.com>
2023-05-12 19:32:23 +0200

    some photos added and displayed, worked on menu

--- pvz.py
+++ pvz.py
@@ -6,14 +6,19 @@ clock = pygame.time.Clock()
 
 BOARD_SIZE_X = 10
 BOARD_SIZE_Y = 5
-SQUARE_SIZE = 60
+SQUARE_SIZE = 120
 SQUARE01_COLOR = (0, 243, 0)
 SQUARE02_COLOR = (0, 145, 0)
-MENU_SIZE = 60
+MENU_SIZE = SQUARE_SIZE
 MENU_COLOR = (100, 100, 250)
+BLACK = (0, 0, 0)
+#image load - potřeba vybrat správnou velikost
+zombie = pygame.image.load("zombie.png")
+zombieKybl = pygame.image.load("zombieKybl.png")
+peashooter = pygame.image.load("peashooter.png")
 
 BULLET_COLOR = (150, 30, 30)
-BULLET_SIZE = 10
+BULLET_SIZE = 30
 bullets = [] # (x,y)
 #vytvoření cvičných kulek
 for index in range(5):
@@ -48,7 +53,14 @@ def draw_board():
             odd = 0
         for index in range(0, BOARD_SIZE_X, 2):
             pygame.draw.rect(window, SQUARE02_COLOR, ((index + odd) * SQUARE_SIZE, (j * SQUARE_SIZE) + MENU_SIZE, SQUARE_SIZE, SQUARE_SIZE))
+    #menu
+    for index in range (3):
+        pygame.draw.rect(window, BLACK, (index * SQUARE_SIZE+1, 2, SQUARE_SIZE-2, SQUARE_SIZE -4 ))
+    window.blit(zombie, (500, 10))
 
+    window.blit(zombieKybl, (300, 480))
+    for index in range(1, 6):
+        window.blit(peashooter, (130,10+ index*120))
 
 def game_input():
     for event in pygame.event.get():
