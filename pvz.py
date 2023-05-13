45989b3f0f3a692bba1018dc295039bb89e96b39
Vojta654 <vojta.kristek05@gmail.com>
2023-05-13 13:37:40 +0200

    board field SQUARE_SIZE_X and SQUARE_SIZE_Y and bullets repaired

--- pvz.py
+++ pvz.py
@@ -6,10 +6,11 @@ clock = pygame.time.Clock()
 
 BOARD_SIZE_X = 10
 BOARD_SIZE_Y = 5
-SQUARE_SIZE = 120
+SQUARE_SIZE_X = 120
+SQUARE_SIZE_Y = 150
 SQUARE01_COLOR = (79, 150, 4)
 SQUARE02_COLOR = (3, 115, 3)
-MENU_SIZE = SQUARE_SIZE
+MENU_SIZE = SQUARE_SIZE_Y
 MENU_COLOR = (100, 100, 250)
 BLACK = (0, 0, 0) # menu buttons backgrounb color
 BULLET_COLOR = (150, 30, 30)
@@ -21,15 +22,13 @@ zombie = pygame.image.load("zombie.png")
 zombieKybl = pygame.image.load("zombieKybl.png")
 peashooter = pygame.image.load("peashooter.png")
 
-window = pygame.display.set_mode((BOARD_SIZE_X * SQUARE_SIZE, (BOARD_SIZE_Y * SQUARE_SIZE) + MENU_SIZE))
+window = pygame.display.set_mode((BOARD_SIZE_X * SQUARE_SIZE_X, (BOARD_SIZE_Y * SQUARE_SIZE_Y) + MENU_SIZE))
 
 
-#vytvoření cvičných kulek
-for index in range(5):
-    bullets.append(( 1.5 * SQUARE_SIZE - BULLET_SIZE/2, (index+0.5)*SQUARE_SIZE + MENU_SIZE - BULLET_SIZE/2))
+
 
 #příprava na umisťování rostlin - bude použit podobný kod jako v gomoku
-plant_loc = [] #buď pole s souřadenicemi rostlin nebo 1 v board[][]
+plant_loc = [] #buď pole s souřadenicemi rostlin nebo 2 v board[][]
 
 
 #vykreslení hrací plochy v listu - možná bude použito k uložení pozic rostlin
@@ -47,26 +46,26 @@ for i in range(BOARD_SIZE_Y):#tam kde bude jedna se dají sekačky
 def draw_board():
     window.fill(SQUARE01_COLOR)
     odd = 0
-    pygame.draw.rect(window, MENU_COLOR, (0, 0, BOARD_SIZE_X * SQUARE_SIZE, MENU_SIZE))
+    pygame.draw.rect(window, MENU_COLOR, (0, 0, BOARD_SIZE_X * SQUARE_SIZE_X, MENU_SIZE))
     for j in range(BOARD_SIZE_Y):
         if odd == 0:
             odd = 1
         else:
             odd = 0
         for index in range(0, BOARD_SIZE_X, 2):
-            pygame.draw.rect(window, SQUARE02_COLOR, ((index + odd) * SQUARE_SIZE, (j * SQUARE_SIZE) + MENU_SIZE, SQUARE_SIZE, SQUARE_SIZE))
+            pygame.draw.rect(window, SQUARE02_COLOR, ((index + odd) * SQUARE_SIZE_X, (j * SQUARE_SIZE_Y) + MENU_SIZE, SQUARE_SIZE_X, SQUARE_SIZE_Y))
     #menu - vykreslení polí, na kterých bude možno vybírat kytky k položení
     for index in range(3):
-        pygame.draw.rect(window, BLACK, (index * SQUARE_SIZE+1, 2, SQUARE_SIZE-2, SQUARE_SIZE - 4))
+        pygame.draw.rect(window, BLACK, (index * SQUARE_SIZE_X+1, 2, SQUARE_SIZE_X-2, SQUARE_SIZE_Y - 4))
 
     #test pokládání rostlin a zombie
     window.blit(peashooter, (10, 10))
     window.blit(zombie, (500, 10))
 
     window.blit(zombieKybl, (300, 480))
-    for index in range(1, 6):
-        window.blit(peashooter, (130,10+ index*120))
 
+    for index in range(1, 6):
+        window.blit(peashooter, (130,10+ index*SQUARE_SIZE_Y))
 
 #input - možnost vybírání a pokládání rostlin
 def game_input():
@@ -78,8 +77,15 @@ def game_input():
 def game_update():
     draw_board()
 
+
+#vytvoření cvičných kulek
+for index in range(5):
+    bullets.append((1.5 * SQUARE_SIZE_X, (index+0.5)*SQUARE_SIZE_Y + MENU_SIZE - BULLET_SIZE/2))
+
+
 # bude voláno pomocí nějakého časovače, možná bude potřeba předat nějkaý parametr
 def create_bullets():#střela se vytvoří na souřadnici rostliny (bude přepočet na střed) a její souřadnice se bullets.append((x,y)), pak budou muset bý mazány
+    global bullets
     for coords in bullets:
         x, y = coords
         pygame.draw.rect(window, BULLET_COLOR, (x, y, BULLET_SIZE, BULLET_SIZE))
