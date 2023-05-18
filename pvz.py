9610e3fd57e85789fec9c084c3c91268a2384240
Vojta654 <vojta.kristek05@gmail.com>
2023-05-18 21:03:22 +0200

    constants renamed unnecessary code removed some coments added

--- pvz.py
+++ pvz.py
@@ -2,20 +2,12 @@ import pygame,sys, os
 import Constants as c
 
 pygame.init()
-
 clock = pygame.time.Clock()
 
-
-
-for i in range(22):
-    img = pygame.image.load(os.path.join("data", "Zombie_" + str(i) + ".png"))
-    c.zombieImages.append(img)
 window = pygame.display.set_mode((c.BOARD_SIZE_X * c.SQUARE_SIZE_X, (c.BOARD_SIZE_Y * c.SQUARE_SIZE_Y) + c.MENU_SIZE))
 
-# příprava na umisťování rostlin - bude použit podobný kod jako v gomoku
-plant_loc = []  # buď pole s souřadenicemi rostlin nebo 2 v board[][]
 
-# vykreslení hrací plochy v listu - možná bude použito k uložení pozic rostlin
+# vykreslení hrací plochy v listu - je použito k uložení pozic rostlin
 board = []
 for y in range(0, c.BOARD_SIZE_Y, 1):
     row = []
@@ -43,15 +35,11 @@ def draw_board():
     for index in range(3):
         pygame.draw.rect(window, c.BLACK, (index * c.SQUARE_SIZE_X + 1, 2, c.SQUARE_SIZE_X - 2, c.SQUARE_SIZE_Y - 4))
 
-    # test pokládání rostlin a zombie
-    window.blit(c.peashooter, (10, 10))
-    window.blit(c.sunflower, (c.SQUARE_SIZE_X, 10))
-    #window.blit(zombie, (500, 10))
-    pygame.draw.rect(window, c.WHITE, c.MONEY_COUNTER)
-    #window.blit(zombieKybl, (300, 480))
+    #menu - obrázky rostlin + box na počítání peněz
+    window.blit(c.peashooterImage, (10, 10))
+    window.blit(c.sunflowerImage, (c.SQUARE_SIZE_X, 10))
+    pygame.draw.rect(window, c.WHITE, c.MONEY_COUNTER_BOX)
 
-
-print(board)
 # input - možnost vybírání a pokládání rostlin
 def game_input():
     for event in pygame.event.get():
@@ -79,54 +67,53 @@ def create_bullets():  # střela se vytvoří na souřadnici rostliny (bude pře
 def move_bullets():  # updatuje polohu střel, je potřeba pole, kde jsou uloženz polohy každé střely - bullets[g
     for index in range(len(c.bullets)):
         x, y = c.bullets[index]
-        c.bullets[index] = x + 1, y
+        c.bullets[index] = x + c.BULLET_SPEED, y
 
 
 def game_output():
     create_bullets()
     move_bullets()
-    showZombie()
+    updateNormalZombie()
     draw_plants()
+print(c.MONEY_COUNTER_BOX)
 
-
-zombieCoords  = []
+#test zobrazení normálních zombíků
+normalZombiesList  = []
 for i in range(1, 6):
-    zombieCoords.append([c.Z_START_LOCATION, i*c.SQUARE_SIZE_Y, 1])
+    normalZombiesList.append([c.ZOMBIE_START_LOCATION, i*c.SQUARE_SIZE_Y, 1, c.NormalZombieHP])#udaje pro jednotlivého zombíka [x souřadnice, y souřadnice, aktuální snímek, životy]
 
-#zombieCoords = [[1000, 300, 1], [1000, 500, 10]]
-def zombie_moveing():  # pohyb zombi, asi bude potřeba vytvořit další funkci na vytváření zombie a pole s jejich lokací
-    pass
-
+#podívá se do board[][], kde jsou uložený pozice rostlin a tyto rostliny zobrazí
 def draw_plants():
     for ind in range(c.BOARD_SIZE_Y):
         line = board[ind]
         for j in range(len(line)):
             square = line[j]
             if square == 2:
-                window.blit(c.sunflower, (j*c.SQUARE_SIZE_X, (ind + 1) * c.SQUARE_SIZE_Y))#sunflower
+                window.blit(c.sunflowerImage, (j*c.SQUARE_SIZE_X, (ind + 1) * c.SQUARE_SIZE_Y))#sunflower
             elif square ==3:
-                window.blit(c.peashooter, (j * c.SQUARE_SIZE_X, (ind + 1) * c.SQUARE_SIZE_Y))#peashooter
-def showZombie():
-    for i in range(len(zombieCoords)):
-        zombie_x = zombieCoords[i][0]
-        zombie_y = zombieCoords[i][1]
-        currentZombieImage = zombieCoords[i][2]
-        currentZombieImage += 1
-        zombie_x -= c.ZOMBIE_SPEED
-        if currentZombieImage == len(c.zombieImages):
-            currentZombieImage = 0
-        zombieCoords[i][0] = zombie_x
-        zombieCoords[i][2] = currentZombieImage
-        window.blit(c.zombieImages[currentZombieImage], (zombie_x, zombie_y))
+                window.blit(c.peashooterImage, (j * c.SQUARE_SIZE_X, (ind + 1) * c.SQUARE_SIZE_Y))#peashooter
 
-currentZombie = 0
+def updateNormalZombie():
+    for i in range(len(normalZombiesList)):
+        zombie_x = normalZombiesList[i][0]
+        zombie_y = normalZombiesList[i][1]
+        currentZombieImage = normalZombiesList[i][2]
+        currentZombieImage += 1 #další snímek v animaci
+        zombie_x -= c.ZOMBIE_SPEED #posunutí do leva
+        if currentZombieImage == len(c.NormalZombieImages):
+            currentZombieImage = 0
+        #uložení změněných hodnot
+        normalZombiesList[i][0] = zombie_x
+        normalZombiesList[i][2] = currentZombieImage
+        #zobrazení
+        window.blit(c.NormalZombieImages[currentZombieImage], (zombie_x, zombie_y))
+
+
 
 while True:
     game_input()
     game_update()
     game_output()
-
-
     pygame.display.flip()
     clock.tick(30)
 
