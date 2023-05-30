e9dbb74c309bfdec3e91b2b85ca8a490adad6aad
vojta_654 <vojta.kristek05@gmail.com>
2023-05-30 09:13:04 +0200

    count_x(), count_y() added - jednodušší počítání potřebných souřadnic

--- pvz.py
+++ pvz.py
@@ -5,7 +5,15 @@ plants = []
 pygame.init()
 clock = pygame.time.Clock()
 
-window = pygame.display.set_mode((c.BOARD_SIZE_X * c.SQUARE_SIZE_X, (c.BOARD_SIZE_Y * c.SQUARE_SIZE_Y) + c.MENU_SIZE))
+def count_x(number_x):
+    return number_x * c.SQUARE_SIZE_X
+
+
+def count_y(number_y):
+    return number_y * c.SQUARE_SIZE_Y
+
+
+window = pygame.display.set_mode((count_x(c.BOARD_SIZE_X), count_y(c.BOARD_SIZE_Y) + c.MENU_SIZE))
 time = 0
 
 # vykreslení hrací plochy v listu - je použito k uložení pozic rostlin
@@ -23,18 +31,17 @@ for i in range(c.BOARD_SIZE_Y):  # tam kde bude jedna se dají sekačky
 def draw_board():
     window.fill(c.SQUARE01_COLOR)
     odd = 0
-    pygame.draw.rect(window, c.MENU_COLOR, (0, 0, c.BOARD_SIZE_X * c.SQUARE_SIZE_X, c.MENU_SIZE))
+    pygame.draw.rect(window, c.MENU_COLOR, (0, 0, count_x(c.BOARD_SIZE_X), c.MENU_SIZE))
     for j in range(c.BOARD_SIZE_Y):
         if odd == 0:
             odd = 1
         else:
             odd = 0
         for index in range(0, c.BOARD_SIZE_X, 2):
-            pygame.draw.rect(window, c.SQUARE02_COLOR, (
-            (index + odd) * c.SQUARE_SIZE_X, (j * c.SQUARE_SIZE_Y) + c.MENU_SIZE, c.SQUARE_SIZE_X, c.SQUARE_SIZE_Y))
+            pygame.draw.rect(window, c.SQUARE02_COLOR, (count_x(index + odd), count_y(j) + c.MENU_SIZE, c.SQUARE_SIZE_X, c.SQUARE_SIZE_Y))
     # menu - vykreslení polí, na kterých bude možno vybírat kytky k položení
     for index in range(3):
-        pygame.draw.rect(window, c.BLACK, (index * c.SQUARE_SIZE_X + 1, 2, c.SQUARE_SIZE_X - 2, c.SQUARE_SIZE_Y - 4))
+        pygame.draw.rect(window, c.BLACK, (count_x(index) + 1, 2, c.SQUARE_SIZE_X - 2, c.SQUARE_SIZE_Y - 4))
 
     #menu - obrázky rostlin + box na počítání peněz
     window.blit(c.peashooterImage, (10, 10))
@@ -42,8 +49,7 @@ def draw_board():
     pygame.draw.rect(window, c.WHITE, c.MONEY_COUNTER_BOX) ##### rámeček s textem
     font = pygame.font.Font('HERMES 1943.ttf', 32)
     text = font.render('Sluníčka', True, c.BLACK)
-    window.blit(text, (c.SQUARE_SIZE_X * 8, 25))
-
+    window.blit(text, (count_x(8), 25))
 
 
 # input - možnost vybírání a pokládání rostlin
@@ -85,7 +91,7 @@ def on_mouse_up(event):
     if y > 0:
         board[y-1][x] = plant_type
         if plant_type == 3:
-            plants.append([(x + 0.6)*c.SQUARE_SIZE_X, (y + 0.2) * c.SQUARE_SIZE_Y, 0])
+            plants.append([count_x(x + 0.6), count_y(y + 0.2), 0])
     plant_type = 0
 
    
@@ -97,7 +103,7 @@ def game_update():
 
 def create_bullets():
     for plant_bullet in plants:
-        if plant_bullet[2]%135 == 0:
+        if plant_bullet[2] % 135 == 0:
             bullets.append([plant_bullet[0], plant_bullet[1]])
         plant_bullet[2] += 1
     
@@ -133,9 +139,9 @@ def draw_plants():
         for j in range(len(line)):
             square = line[j]
             if square == 2:
-                window.blit(c.sunflowerImage, (j*c.SQUARE_SIZE_X, (ind + 1) * c.SQUARE_SIZE_Y+10))#sunflower
+                window.blit(c.sunflowerImage, (count_x(j), count_y(ind + 1)+10))#sunflower
             elif square ==3:
-                window.blit(c.peashooterImage, (j * c.SQUARE_SIZE_X+10, (ind + 1) * c.SQUARE_SIZE_Y+20))#peashooter
+                window.blit(c.peashooterImage, (count_x(j)+10, count_y(ind + 1)+20))#peashooter
 
 
 def updateNormalZombie():
@@ -156,7 +162,7 @@ def updateNormalZombie():
 
 
 def create_normal_zombie(lineNum):
-    normalZombiesList[lineNum].append([c.ZOMBIE_START_LOCATION, (lineNum+1)*c.SQUARE_SIZE_Y, 1, c.NormalZombieHP])#udaje pro jednotlivého zombíka [x souřadnice, y souřadnice, aktuální snímek, životy]
+    normalZombiesList[lineNum].append([c.ZOMBIE_START_LOCATION, count_y(lineNum+1), 1, c.NormalZombieHP])#udaje pro jednotlivého zombíka [x souřadnice, y souřadnice, aktuální snímek, životy]
 
 def gamelevel_one():
     if round(time, 2) == 10:
